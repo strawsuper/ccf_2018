b7329098ad29252349c9dfc9681fc45c366917a4
strawsuper <471814775@qq.com>
2018-09-21 21:52:29 +0800

    9.21_init

---
+++ 面向电信行业存量用户的智能套餐个性化匹配模型/code/群/简单的基于sub的融合投票.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/9/11 12:06 AM
+# @Author  : Inf.Turing
+# @Site    : 
+# @File    : 简单的基于sub的融合投票.py
+# @Software: PyCharm
+
+# 不要浪费太多时间在自己熟悉的地方，要学会适当的绕过一些
+# 良好的阶段性收获是坚持的重要动力之一
+# 用心做事情，一定会有回报
